2f05387e8fd9a13f8bdc3f1e745a87581bec6372
oguz gül <ogzgl95@outlook.com>
2021-08-23 16:32:24 +0300

    Create feed component and build feed section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </div>
     </div>
