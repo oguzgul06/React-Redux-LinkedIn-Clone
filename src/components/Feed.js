992bbfdf6eaba1ef45e5206e74b1999430657f6e
oguz gül <ogzgl95@outlook.com>
2021-08-24 12:34:53 +0300

    Build the Post Section

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
 import InputOption from "./InputOption";
-import ImageIcon from '@material-ui/icons/Image';
-import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post";
 
 function Feed() {
+
+  const [posts, setPosts] = useState([]);
+
+  const sendPost = (event) => {
+      event.preventValoue();
+  }
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -15,16 +23,29 @@ function Feed() {
           <CreateIcon />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
         <div className="feed__inputOptions">
-          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
-          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
-          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
-          <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
         </div>
       </div>
+
+      {posts.map((post) => (
+          <Post />
+      ))}
+      <Post
+        name="Oğuz GÜL"
+        description="This is a test"
+        message="Woow this worked"
+      />
     </div>
   );
 }
